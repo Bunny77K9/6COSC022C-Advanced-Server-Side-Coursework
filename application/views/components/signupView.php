94b60ae444e45d8f8d03b5a5120c179e0f20a56d
Nandun Diaz <nadundias321@gmail.com>
2024-05-16 07:58:53 +0530

    fixed minor bugs in login and signup

--- application/views/components/signupView.php
+++ application/views/components/signupView.php
@@ -1,63 +1,55 @@
 <script type="text/template" id="signup-template">
 	<div class="row no-gutters">
-		<div class="col-sm-8 mx-auto">
-			<h1 class="text-center">Dev Forum</h1>
+		<div class="col-sm-8 mx-auto loin-signup">
+			<h1 class="title text-center">DEV FORUM</h1>
 			<div class="card card-body my-5">
 				<h2 class="card-title text-center">Sign up</h2>
 				<form class="form-main">
 
 					<p class="error text-center" id="signup-error"></p>
 
-					<div class="container">
-						<div class="row form-input-signup">
-							<div class="col-sm-6">
-								<input type="text" class="form-control" placeholder="First name"
-									   required id="signupFirstname">
-							</div>
-							<div class="col-sm-6">
-								<input type="text" class="form-control" placeholder="Last name"
-									   required id="signupLastname">
-							</div>
+					<div class="row form-input-signup mb-4">
+						<div class="col-sm-6">
+							<input type="text" class="form-control" placeholder="First name"
+								   required id="signupFirstname" aria-label="Firstname">
+						</div>
+						<div class="col-sm-6">
+							<input type="text" class="form-control" placeholder="Last name"
+								   required id="signupLastname" aria-label="Lastname">
 						</div>
 					</div>
 
-					<div class="container">
-						<div class="row form-input-signup">
-							<div class="col-sm-12">
-								<input type="email" class="form-control" placeholder="Email address"
-									   required id="signupEmail">
-							</div>
+					<div class="row form-input-signup mb-4">
+						<div class="col-sm-12">
+							<input type="email" class="form-control" placeholder="Email address"
+								   required id="signupEmail" aria-label="Email">
 						</div>
 					</div>
 
-					<div class="container">
-						<div class="row form-input-signup">
-							<div class="col-sm-12">
-								<input type="text" class="form-control" placeholder="Title"
-									   required id="signupOccupation">
-							</div>
+					<div class="row form-input-signup mb-4">
+						<div class="col-sm-12">
+							<input type="text" class="form-control" placeholder="Title"
+								   required id="signupOccupation" aria-label="Title">
 						</div>
 					</div>
 
-					<div class="container">
-						<div class="row form-input-signup">
-							<div class="col-sm-6">
-								<div class="input-group">
-									<span class="input-group-text" id="basic-addon1">@</span>
-									<input type="text" class="form-control" placeholder="Username"
-										   aria-label="Username" aria-describedby="basic-addon1" required
-										   id="signupUsername">
-								</div>
+					<div class="row form-input-signup mb-4">
+						<div class="col-sm-6">
+							<div class="input-group">
+								<span class="input-group-text" id="basic-addon1">@</span>
+								<input type="text" class="form-control" placeholder="Username"
+									   aria-label="Username" aria-describedby="basic-addon1" required
+									   id="signupUsername">
 							</div>
-							<div class="col-sm-6">
-								<div class="input-group">
+						</div>
+						<div class="col-sm-6">
+							<div class="input-group">
 								<span class="input-group-text" id="basic-addon2"><i
 										class="fa-solid fa-key"></i></span>
-									<input type="password" id="signupPassword" class="form-control"
-										   placeholder="Password"
-										   aria-label="Username" aria-describedby="basic-addon2"
-										   required name="password">
-								</div>
+								<input type="password" id="signupPassword" class="form-control"
+									   placeholder="Password"
+									   aria-label="Username" aria-describedby="basic-addon2"
+									   required name="password">
 							</div>
 						</div>
 					</div>
