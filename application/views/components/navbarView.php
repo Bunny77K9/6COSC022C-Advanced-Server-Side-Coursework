38dd5b9be4f0611dc3ebc529545e7af30163d2d4
Nandun Diaz <nadundias321@gmail.com>
2024-05-16 09:26:53 +0530

    add logout button

--- application/views/components/navbarView.php
+++ application/views/components/navbarView.php
@@ -14,7 +14,7 @@
 				<form class="d-flex justify-content-center flex-grow-1 me-auto mb-2 mb-lg-0 search-form"
 					  role="search">
 					<input class="form-control me-2" id="search-question-input" type="search"
-						   placeholder="&#128269; Search Question" aria-label="Search">
+						   placeholder="&#128269; Search Questions" aria-label="Search">
 					<button class="btn btn-outline-secondary search-btn" id="search-question" type="submit">Search
 					</button>
 				</form>
